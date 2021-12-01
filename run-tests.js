c6268f9edfa99d677989b12a98046142faa80bb3
Chris Breiding <chrisbreiding@gmail.com>
2021-12-01 14:47:42 -0500

    run test with module api and check results for aberrations

---
+++ run-tests.js
@@ -0,0 +1,37 @@
+const cypress = require('../cypress/cli')
+
+const aberrations = []
+
+cypress.run({
+  browser: 'electron',
+  parallel: true,
+  record: true,
+  group: 'all tests',
+  ciBuildId: process.env.CI_BUILD_ID,
+  key: process.env.CYPRESS_RECORD_KEY,
+})
+.then((results) => {
+  results.runs.forEach((run) => {
+    run.hooks.forEach((hook) => {
+      if (!hook.title || !hook.title.length || !hook.body) {
+        aberrations.push(hook)
+      }
+    })
+    run.tests.forEach((test) => {
+      if (!test.title || !test.title.length || !test.body) {
+        aberrations.push(test)
+      }
+    })
+  })
+
+  if (aberrations.length) {
+    console.log('--- ABERRATIONS ---')
+    console.log(aberrations)
+    process.exit(1)
+  }
+})
+.catch((err) => {
+  console.log('--- RUN ERROR ---')
+  console.log(err.stack)
+  process.exit(1)
+})
